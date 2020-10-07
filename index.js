c8e805a3d94bc2df1fb2ffba803d936f6d321a56
Yisroel Arnson <yisroelrnsn@gmail.com>
2020-10-07 15:28:22 +0300

    ad

--- index.js
+++ index.js
@@ -9,25 +9,23 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.static("public"))
 
-// let baseUrl = "https://spotifyplaylistcreator.herokuapp.com"
-let baseUrl = "http://localhost:5000"
+let baseUrl = "https://spotifyplaylistcreator.herokuapp.com"
+// let baseUrl = "http://localhost:5000"
 app.get('/auth', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/tokenpage.html'));
-  // var scopes = 'user-library-read playlist-modify-public playlist-modify-private user-read-email';
-  // var my_client_id = 'cefa8cc2bdd94621be08b7ba3a4b4142';
-  // var redirect_uri = baseUrl + '/token'
-  // res.redirect('https://accounts.spotify.com/authorize' +
-  //   '?response_type=code' +
-  //   '&client_id=' + my_client_id +
-  //   (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-  //   '&redirect_uri=' + encodeURIComponent(redirect_uri));
+  var scopes = 'user-library-read playlist-modify-public playlist-modify-private user-read-email';
+  var my_client_id = 'cefa8cc2bdd94621be08b7ba3a4b4142';
+  var redirect_uri = baseUrl + '/token'
+  res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + my_client_id +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri));
 });
 
 
 
 app.get('/', function (req, res) {
   res.send('hello')
-  // res.sendFile(path.join(__dirname + '/public/tokenpage.html'));
 });
 
 
