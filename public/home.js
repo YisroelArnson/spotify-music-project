b0751a882df0c1e543b80965aaec3797b0ccc17a
yisroelarnson <yisroel.arnson@wonderlic.com>
2021-07-19 17:22:11 -0400

    started developing new home page (dark theme). I made the banner and top info section. Almost done with top info section

--- public/home.js
+++ public/home.js
@@ -4,18 +4,19 @@ var insight;
 var popSong;
 
 async function onLoad() {
-//Fetch Insight, display content section, display insight
+    //Fetch Insight, display content section, display insight
     console.log('Fetching insight')
     insight = await fetchInsight()
     console.log('Insight fetched: ' + insight)
     // displayContentSection()
     displayInsight(insight)
-//Fetch access token so that I can fetch the popular song using the ID I got from insight
+    displaySongCount(insight)
+    //Fetch access token so that I can fetch the popular song using the ID I got from insight
     console.log('Fetching Access Token')
     accessTokenData = await getAccessToken()
     accessToken = accessTokenData.access_token
     console.log('Access Token fetched: ' + accessToken)
-//Query spotify API to fetch popular song and display the popular song
+    //Query spotify API to fetch popular song and display the popular song
     console.log("Fetching pop_song")
     popSong = await fetchPopSong()
     console.log("Pop_song fetched: " + popSong)
@@ -25,19 +26,19 @@ async function onLoad() {
 async function fetchInsight() {
     return new Promise((resolve, reject) => {
 
-    const insightUrl = baseUrl + 'insights/insight'
-    
-    fetch(insightUrl, {
-                method: 'GET',
-            }).then(response => {
-                return response.json()  
-            }).then(data => {
-                resolve(data)
-            }).catch(err => {
-                console.log(err)
-                reject(err)
-            })
+        const insightUrl = baseUrl + 'insights/insight'
+
+        fetch(insightUrl, {
+            method: 'GET',
+        }).then(response => {
+            return response.json()
+        }).then(data => {
+            resolve(data)
+        }).catch(err => {
+            console.log(err)
+            reject(err)
         })
+    })
 }
 
 async function getAccessToken() {
@@ -48,14 +49,14 @@ async function getAccessToken() {
                 'content-type': 'application/json',
             },
         }).then(response => {
-            return response.json()  
+            return response.json()
         }).then(data => {
-                resolve(data)
-            })
-        .catch(err => {
-            console.log(err)
-            reject(err)
+            resolve(data)
         })
+            .catch(err => {
+                console.log(err)
+                reject(err)
+            })
     })
 }
 
@@ -66,6 +67,11 @@ function displayContentSection() {
 
 function displayInsight(insightData) {
     document.getElementById('insight').innerHTML = JSON.stringify(insightData);
+}
+
+function displaySongCount(insightData) {
+    var num = insightData.total_song_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    document.getElementById('song-count-text').innerHTML = num;
 }
 
 function displayPopSong(PopSongData) {
