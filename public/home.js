fe6f40188c3b86198cae009088b3b3e6479b40af
yisroelarnson <yisroel.arnson@wonderlic.com>
2022-04-19 20:13:38 -0400

    finished boxes 1-6 on homepage

--- public/home.js
+++ public/home.js
@@ -10,7 +10,7 @@ async function onLoad() {
     console.log('Insight fetched: ' + insight)
     // displayContentSection()
     console.log(insight)
-    displayInsight(insight)
+    // displayInsight(insight)
     displaySongCount(insight)
     //Fetch access token so that I can fetch the popular song using the ID I got from insight
     console.log('Fetching Access Token')
@@ -25,6 +25,11 @@ async function onLoad() {
     // displayPopSong(popSong) DELETE SOON
     displayTopSong(insight)
     displayTopGenres(insight)
+    displayTopYearRelease(insight)
+
+    popArtist = await fetchPopArtist(insight.pop_artists);
+    popArtistTopSongs = await fetchArtistTopSongs(popArtist)
+    displayTopArtist(popArtist, popArtistTopSongs)
 }
 
 async function fetchInsight() {
@@ -69,9 +74,9 @@ function displayContentSection() {
     document.getElementById('content-section').style.display = 'block'
 }
 
-function displayInsight(insightData) {
-    document.getElementById('insight').innerHTML = JSON.stringify(insightData);
-}
+// function displayInsight(insightData) {
+//     document.getElementById('insight').innerHTML = JSON.stringify(insightData);
+// }
 
 function displaySongCount(insightData) {
     var num = insightData.total_song_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -83,6 +88,10 @@ function displayTopSong(insightData) {
     document.getElementById('top-song-artist').innerHTML = insightData.pop_song.artists[0].name;
     document.getElementById('top-song-genres').innerHTML = toTitleCase(insightData.pop_song.genres.join(", "));
     document.getElementById('top-song-popularity').innerHTML = insightData.pop_song.popularity + " / 100";
+}
+
+function displayTopArtist() {
+
 }
 
 function toTitleCase(str) {
@@ -105,22 +114,103 @@ function displayTopGenres(insightData) {
         });
         return requiredObj;
      };
-    let topGenres = pickHighest(insight.pop_genres, 3);
-    let topGenresStr = Object.keys(topGenres).join(", ");
+    let topGenres = pickHighest(insightData.pop_genres, 3);
+    let topGenresStr = Object.keys(topGenres);
 
 
-
-    document.getElementById('top-category-genres').innerHTML = toTitleCase(topGenresStr);
+    document.getElementById('top-genre-1').innerHTML = toTitleCase(topGenresStr[0]);
+    document.getElementById('top-genre-2').innerHTML = toTitleCase(topGenresStr[1]);
+    document.getElementById('top-genre-3').innerHTML = toTitleCase(topGenresStr[2]);
 }
 
+function displayTopYearRelease(insighData) {
+    const pickHighest = (obj, num = 3) => {
+        const requiredObj = {};
+        if(num > Object.keys(obj).length){
+           return false;
+        };
+        Object.keys(obj).sort((a, b) => obj[b] - obj[a]).forEach((key, ind) =>
+        {
+           if(ind < num){
+              requiredObj[key] = obj[key];
+           }
+        });
+        return requiredObj;
+     };
+    let topYears = pickHighest(insighData.pop_year_release, 3);
+    let topYearsStr = Object.keys(topYears);
+
+    document.getElementById('top-year-release-1').innerHTML = toTitleCase(topYearsStr[2]);
+    document.getElementById('top-year-release-2').innerHTML = toTitleCase(topYearsStr[1]);
+    document.getElementById('top-year-release-3').innerHTML = toTitleCase(topYearsStr[0]);
+}
 
 function displayPopSong(PopSongData) {
     document.getElementById('pop-song').innerHTML = JSON.stringify(PopSongData);
 }
 
+function displayTopArtist(artist, topTracks) {
+    console.log(artist)
+    console.log(topTracks)
+
+    document.getElementById('top-artist-name').innerHTML = artist.name;
+    document.getElementById('top-artist-pop').innerHTML = artist.popularity + " / 100";
+    for(let i = 0; i < 5; i++) {
+        document.getElementById('song-table-container').innerHTML += 
+        `
+        <a href="${topTracks.tracks[i].external_urls.spotify}" target="_blank">
+        <div class="song-table-row">
+            <h9>${topTracks.tracks[i].name}</h9>
+                <h9 class="e7">${topTracks.tracks[i].album.name}</h9>
+                <h9 class="e7">${topTracks.tracks[i].popularity + "/ 100"}</h9>
+        </div>
+        </a>
+        `
+    }
+
+
+}
+
 async function fetchPopSong() {
     return new Promise((resolve, reject) => {
         fetch('https://api.spotify.com/v1/tracks/' + insight.pop_song.song_id, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            resolve(data)
+        }).catch(err => {
+            console.log(err)
+            reject(err)
+        })
+    })
+}
+
+async function fetchPopArtist(topArtists) {
+    topArtist = Object.keys(topArtists).reduce(function(a, b){ return topArtists[a] > topArtists[b] ? a : b });
+    return new Promise((resolve, reject) => {
+        fetch('https://api.spotify.com/v1/artists/' + topArtist, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            resolve(data)
+        }).catch(err => {
+            console.log(err)
+            reject(err)
+        })
+    })
+}
+
+async function fetchArtistTopSongs(artist) {
+    return new Promise((resolve, reject) => {
+        fetch('https://api.spotify.com/v1/artists/' + artist.id + "/top-tracks?market=US", {
             method: 'GET',
             headers: {
                 'Authorization': 'Bearer ' + accessToken
